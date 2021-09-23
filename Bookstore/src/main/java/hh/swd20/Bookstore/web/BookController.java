7d29c9f806bd13d6280f5366eba927e3eafe6d81
Julia Söderlund <julia.soderlund@yandex.ru>
2021-09-23 09:58:13 +0300

    Added edit and delete functions

--- Bookstore/src/main/java/hh/swd20/Bookstore/web/BookController.java
+++ Bookstore/src/main/java/hh/swd20/Bookstore/web/BookController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -17,11 +18,6 @@ public class BookController {
 	@Autowired
 	private BookRepository repository;
 	
-	//tätä ei varmaan enää tarvitse
-	@RequestMapping(value = "/index", method = RequestMethod.GET)
-	public String bookPage() {
-		return("bookstore");
-	}
 	
 	@RequestMapping(value = "/booklist", method = RequestMethod.GET)
 	public String listBooks(Model model) {
@@ -29,4 +25,28 @@ public class BookController {
 		model.addAttribute("books", books); //välitetään tyhjä kirjalista 
 		return "booklist";
 	}
+	
+	@RequestMapping(value = "/add")
+    public String addBook(Model model){
+    	model.addAttribute("book", new Book());
+        return "addbook";
+    } 
+	
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public String editBook(@PathVariable("id") Long bookId, Model model){
+		model.addAttribute("book", repository.findById(bookId).get());
+        return "editbook";
+    }
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Book book){
+        repository.save(book);
+        return "redirect:booklist";
+    }  
+	
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+    	repository.deleteById(bookId);
+        return "redirect:../booklist";
+    }   
 }
